404208b9c27bb3f58bdbc260f688b07d28bf5abf
RUstam <exellentgo3@gmail.com>
2020-09-30 21:28:56 +0500

    feat: min,max,avg completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length == 0){
+        return 0;
+    }
+    else {
+        return Math.min.apply(null, array);
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length == 0){
+        return 0;
+    }
+    else {
+        return Math.max.apply(null, array);
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length == 0){
+        return 0;
+    }
+    else {
+        var x = 0;
+        for (var i = 0; i<array.length; i++){
+            x = x + array[i];
+        }
+        x = x/array.length;
+        return x;
+    }
 }
